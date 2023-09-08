be39d5fc366be0d37dd08e5de4f8acaaaa95e4d8
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-08 16:02:32 -0400

    setting everything up

---
+++ src/AssembleRangers.js
@@ -0,0 +1,14 @@
+import React from "react";
+import rangers from "../rangerRoster/coconutAvengers"
+import rangers from "../rangerRoster/soggyPillows"
+import rangers from "../rangerRoster/tickleElves"
+
+function AssembleRangers() {
+    // combine all the rangers arrays form rangerRoster into one large array
+    
+    return (
+        <></>
+    )
+}
+
+export default AssembleRangers;
