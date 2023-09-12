f9a70a0fb9d2223824c70f4518af803936f141d4
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-12 12:02:28 -0400

    trying to get others to show up

--- src/AssembleRangers.js
+++ src/AssembleRangers.js
@@ -1,35 +1,37 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import RangerCard from "./RangerCard";
 
 function AssembleRangers() {
-    // combine all the rangers from rangerRoster into one large array
-    // converts the json into something we can work with
- const [rangers, setRangers] = useState([]);
- console.log(rangers)
+  // combine all the rangers from rangerRoster into one large array
+  // converts the json into something we can work with
+  const [coconuts, setCoconuts] = useState([]);
+  const [soggies, setSoggies] = useState([]);
+  const [ticklers, setTicklers] = useState([]);
 
-    useEffect(() => {
-        fetch(`http://localhost:4000/coconutAvengers`)
-        .then((r) => r.json())
-        .then((rangers)=> setRangers(rangers))
-      }, []);
+  useEffect(() => {
+    fetch(`http://localhost:4000/coconutAvengers`)
+      .then((r) => r.json())
+      .then((rangersJson) => setCoconuts(rangersJson)); 
+      // setRangers set the current state of rangers to rangersJson
+  }, []);
 
-      // useEffect(() => {
-      //   fetch(`http://localhost:4000/soggyPillows`)
-      //   .then((r) => r.json())
-      //   .then((rangers)=> setRangers(rangers))
-      // }, []);
+  // useEffect(() => {
+  //   fetch(`http://localhost:4000/soggyPillows`)
+  //   .then((r) => r.json())
+  //   .then((rangers)=> setRangers(rangers))
+  // }, []);
 
-      // useEffect(() => {
-      //   fetch(`http://localhost:4000/tickleElves`)
-      //   .then((r) => r.json())
-      //   .then((rangers)=> setRangers(rangers))
-      // }, []);
+  // useEffect(() => {
+  //   fetch(`http://localhost:4000/tickleElves`)
+  //   .then((r) => r.json())
+  //   .then((rangers)=> setRangers(rangers))
+  // }, []);
 
-//const rangerDisplay = rangers;
-    return (
-        <div>
-            Recruit 5 rangers
-            {/* should show a bunch of ranger cards for
+  //const rangerDisplay = rangers;
+  return (
+    <div>
+      Recruit 5 rangers
+      {/* should show a bunch of ranger cards for
             us to select, is when we recruit, the text should be
             updated with the remaining number of rangers that are needed to
             form the megazord
@@ -39,9 +41,13 @@ function AssembleRangers() {
             one object in new array would be a rangerCard 
             
             rangers.map((ranger)=> {return <RangerCard/>})*/}
-           {rangers.map((ranger) => {return <RangerCard rangerName={ranger.name} rangerColor={ranger.color}/>})}
-        </div>
-    )
+      {coconuts.map((ranger) => {
+        return (
+          <RangerCard rangerName={ranger.name} rangerColor={ranger.color} />
+        );
+      })}
+    </div>
+  );
 }
 
 export default AssembleRangers;
--- src/RangerCard.js
+++ src/RangerCard.js
@@ -1,29 +1,22 @@
 import React, { useState } from "react";
 
+function RangerCard({ rangerColor, rangerName }) {
+  const [isCardHidden, setIsCardHidden] = useState(false);
 
-function RangerCard({rangerColor, rangerName}) {
+  if (isCardHidden) {
+    return null;
+  }
 
-    // useEffect(() => {
-    //     fetch(`http://localhost:4000/${rangerTeam}`)
-    //     .then((r) => r.json)
-    //     .then((rangers)=> setRangers(rangers))
-    //   }, []);
-      
-    const [isCardHidden, setIsCardHidden] = useState(false);
-    // const {name, color} = ranger;
-
-    if (isCardHidden) {
-        return null;
-    }
-
-    return (
-        <div className="card">
-            <h3>name:{rangerName}</h3>
-            <div>color:{rangerColor}</div>
-            <button className="ui button"
-            onClick={() => setIsCardHidden(true)}></button>
-        </div>
-    )
+  return (
+    <div className="card">
+      <h3>name:{rangerName}</h3>
+      <div>color:{rangerColor}</div>
+      <button
+        className="ui button"
+        onClick={() => setIsCardHidden(true)}
+      ></button>
+    </div>
+  );
 }
 
 export default RangerCard;
