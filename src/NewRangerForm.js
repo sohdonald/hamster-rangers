ef57422a6aec9478ba603d7f2340717ab915ca6e
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-25 21:55:48 -0400

    made the select elements

--- src/NewRangerForm.js
+++ src/NewRangerForm.js
@@ -5,67 +5,86 @@
 
 import React, { useState } from "react";
 
-
 function NewRangerForm() {
-    const [newName, setNewName] = useState("")
-    const [newColor, setNewColor] = useState("red")
-    const [newTeam, setNewTeam] =useState("")
-    const [newRanger, setNewRanger] =useState("")
+  const [newName, setNewName] = useState("");
+  const [newColor, setNewColor] = useState("red");
+  const [newTeam, setNewTeam] = useState("Soggy Pillows");
+  const [newRanger, setNewRanger] = useState("");
 
-// function handleNewRanger() {
-// setNewRanger([...rangers, newRanger]);
+  // function handleNewRanger() {
+  // setNewRanger([...rangers, newRanger]);
 
-
-function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
     const newRangerData = {
-        name: newName,
-        color: newColor,
-        team: newTeam,
-    }
+      name: newName,
+      color: newColor,
+      team: newTeam,
+    };
     console.log(newRangerData);
 
     fetch(`http://localhost:4000/rangers`, {
-        method: "POST",
-        headers: {"Content-Type": "application/json",},
-        body: JSON.stringify(newRangerData),
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newRangerData),
     })
-    .then((r) => r.json())
-    .then((rangerSubmit) => setNewRanger(rangerSubmit))
-}
+      .then((r) => r.json())
+      .then((rangerSubmit) => setNewRanger(rangerSubmit));
+  }
 
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label> Name:
-                <input 
-                type="text"
-                name="name"
-                value={newName}
-                onChange={(e)=> setNewName(e.target.value)}
-                />
-                </label>
-                <label> Color:
-                <input 
-                type="text"
-                name="color"
-                value={newColor}
-                onChange={(e)=> setNewColor(e.target.value)}
-                />
-                </label>  
-                <label> Team:
-                <input 
-                type="text"
-                name="team"
-                value={newTeam}
-                onChange={(e)=> setNewTeam(e.target.value)}
-                />
-                </label>
-                <button type="submit">Recruit</button>
-                <h2>Bob has been recruited!</h2>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          {" "}
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </label>
+        <label>
+          {" "}
+          Color:
+          <select
+            id="submitColor"
+            type="text"
+            name="color"
+            value={newColor}
+            // change this into select html element
+            onChange={(e) => setNewColor(e.target.value)}
+          >
+            <option value="red">red</option>
+            <option value="blue">blue</option>
+            <option value="green">green</option>
+            <option value="yellow">yellow</option>
+            <option value="pink">pink</option>
+          </select>
+        </label>
+        <label>
+          Team:
+          <select
+            id="submitTeam"
+            type="text"
+            name="team"
+            value={newTeam}
+            // let's change this into a select html element
+            onChange={(e) => setNewTeam(e.target.value)}
+          >
+            <option value="Soggy Pillows">Soggy Pillows</option>
+            <option value="Tickle Elves">Tickle Elves</option>
+            <option value="Clean Towels">Clean Towels</option>
+            <option value="Fuzzy Pumpkins">Fuzzy Pumpkins</option>
+            <option value="Coconut Avengers">Coconut Avengers</option>
+          </select>
+        </label>
+        <button type="submit">Recruit</button>
+        <h2>Bob has been recruited!</h2>
+      </form>
+    </div>
+  );
 }
 
 export default NewRangerForm;
