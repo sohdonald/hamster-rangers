9e6e401946fffefb6d9e7173a2074b8ad9c70556
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-27 10:59:24 -0400

    making reset button

--- src/Filter.js
+++ src/Filter.js
@@ -17,18 +17,6 @@ function Filter({ selectedTeam, onTeamChange}) {
         <option value="Clean Towels">Clean Towels</option>
         <option value="Fuzzy Pumpkins">Fuzzy Pumpkins</option>
       </select>
-      {/* <select
-        name="filter-color"
-        value={selectedColor}
-        onChange={(e) => onColorChange(e.target.value)}
-      >
-        <option value="All">Filter by Color</option>
-        <option value="red">red</option>
-        <option value="blue">blue</option>
-        <option value="yellow">yellow</option>
-        <option value="green">green</option>
-        <option value="pink">pink</option>
-      </select> */}
     </>
   );
 }
